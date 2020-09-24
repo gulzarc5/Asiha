14130bfa539253446e5728c8897d321bb577f426
gulzarc5 <gulzarc5@gmail.com>
2020-09-24 13:06:55 +0530

    update

--- resources/views/admin/product/edit_size.blade.php
+++ resources/views/admin/product/edit_size.blade.php
@@ -29,13 +29,12 @@
     	            <div class="x_content">
                         @if (isset($product))
                             <div id="product_size_add_form">
-                                {{ Form::open(['method' => 'post','route'=>'admin.product_add_new_sizes']) }}
-                                    <input type="hidden" name="product_id" value="{{$product->id}}">
+                                {{ Form::open(['method' => 'put','route'=>['admin.product_add_new_sizes','product_id'=>$product->id]]) }}
                                     <div class="well" style="overflow: auto" id="size_div">
-                                        <div class="form-row mb-3">                                
+                                        <div class="form-row mb-3">
                                             <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
                                                 <label for="size">Select Size <span><b style="color: red"> * </b></span></label>
-                                                <select class="form-control size_option" name="size[]"  required>
+                                                <select class="form-control size_option" name="size[]" id="size_option"  required>
                                                   <option value="">Select Size</option>
                                                     @if (isset($sizes) && !empty($sizes))
                                                         @foreach ($sizes as $size)
@@ -52,7 +51,7 @@
                                                 <label for="mrp">Enter M.R.P. <span><b style="color: red"> * </b></span></label>
                                                 <input type="number" step="any" class="form-control" name="mrp[]"  placeholder="Enter MRP" required>
                                             </div>
-            
+
                                             <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
                                                 <label for="price">Enter Price <span><b style="color: red"> * </b></span></label>
                                                 <input type="number" step="any" class="form-control" name="price[]"  placeholder="Enter Price" required>
@@ -62,7 +61,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group">    
+                                    <div class="form-group">
                                         <button type="submit" class='btn btn-success'>Submit</button>
                                         <button type="button" class='btn btn-warning' id="size_add_form_back_btn">Back</button>
                                     </div>
@@ -87,6 +86,7 @@
                                             @foreach ($product->sizes as $item)
                                                 <tr>
                                                     <td>
+                                                        <input type="hidden" name="size_id[]" value="{{$item->id}}">
                                                         <select class="form-control size_option" name="size[]"  required>
                                                             <option value="">Select Size</option>
                                                             @if (isset($sizes) && !empty($sizes))
@@ -95,12 +95,12 @@
                                                                     <option value="{{$size->id}}" selected>{{$size->name}}</option>
                                                                     @else
                                                                     <option value="{{$size->id}}">{{$size->name}}</option>
-                                                                    @endif                                                                    
+                                                                    @endif
                                                                 @endforeach
                                                             @endif
                                                         </select>
                                                     </td>
-                                                    <td> 
+                                                    <td>
                                                         <input type="number" class="form-control" name="stock[]"  placeholder="Enter Product Stock" value="{{$item->stock}}"  required>
                                                     </td>
                                                     <td>
@@ -110,21 +110,21 @@
                                                         <input type="number" class="form-control" name="price[]"  placeholder="Enter price" value="{{$item->price}}">
                                                     </td>
                                                 </tr>
-                                            @endforeach      
+                                            @endforeach
                                             <tr>
                                                 <td colspan="8" align="center">
                                                     <button type="button" class="btn btn-sm btn-primary" id="add_more_size_btn">Add New Size</button>
                                                     <button type="submit" class='btn btn-success'>Update Size</button>
                                                     <button class="btn btn-danger" onclick="window.close();">Close Window</button>
                                                 </td>
-                                            </tr>                      
+                                            </tr>
                                         </tbody>
                                         </table>
                                         {{ Form::close() }}
-                                    </div>                    
+                                    </div>
                                 @endif
-                                
-                            </div>  
+
+                            </div>
                         @endif
     	            </div>
     	        </div>
@@ -140,59 +140,36 @@
 @section('script')
 <script>
     var size_div_count = 1;
-    $(function() {  
+    $(function() {
+        var size_option = $("#size_option").html();
         $("#product_size_add_form").hide();
         $(document).on('click',"#add_more_size", function(){
-            var size_html = `<br><div class="form-row mb-3">
+            var size_html = `<div id="sizes${size_div_count}"><div class="form-row mb-3">
                 <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="size">Enter Size Name</label>
-                    <input type="text" class="form-control" name="size[]"  placeholder="Enter Size Name" required>
+                    <label for="size">Select Size <span><b style="color: red"> * </b></span></label>
+                    <select class="form-control size_option" name="size[]"  required>
+                    ${size_option}
+                    </select>
                 </div>
 
                 <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="mrp">Enter Product M.R.P.</label>
-                    <input type="number" class="form-control" name="mrp[]"  placeholder="Enter Product M.R.P." required>
+                    <label for="stock">Enter Stock <span><b style="color: red"> * </b></span></label>
+                    <input type="number" step="any" class="form-control" name="stock[]"  placeholder="Enter Stock" min="1" value="1" required>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
+                    <label for="mrp">Enter M.R.P. <span><b style="color: red"> * </b></span></label>
+                    <input type="number" step="any" class="form-control" name="mrp[]"  placeholder="Enter MRP" required>
                 </div>
 
                 <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="customer_price">Enter Customer Price.</label>
-                    <input type="number" class="form-control" name="customer_price[]"  placeholder="Enter Customer Price" required>
+                    <label for="price">Enter Price <span><b style="color: red"> * </b></span></label>
+                    <input type="number" step="any" class="form-control" name="price[]"  placeholder="Enter Price" required>
                 </div>
 
-            </div>
-            
-            <div class="form-row mb-3">
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="retailer_price">Enter Retailer Price.</label>
-                    <input type="number" class="form-control" name="retailer_price[]"  placeholder="Enter Retailer Price" required>
+                <div class="col-md-8 col-sm-12 col-xs-12 mb-3" style="margin-top: 25px;">
+                    <button type="button" class="btn btn-sm btn-danger"  onclick="removeSize(${size_div_count})">Remove</button>
                 </div>
-
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="customer_discount">Enter Customer Discount.</label>
-                    <input type="number" class="form-control" name="customer_discount[]"  placeholder="Enter Customer Discount" >
-                </div>
-
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="retailer_discount">Enter Retailer Discount.</label>
-                    <input type="number" class="form-control" name="retailer_discount[]"  placeholder="Enter Retailer Discount" >
-                </div>
-            </div>
-
-            <div class="form-row mb-3">
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                <label for="min_order_quantity">Retailer Min Order Quantity</label>
-                <input type="number" class="form-control" name="min_order_quantity[]"  placeholder="Enter Retailer Min Order Quantity"  required>
-                </div>
-
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <label for="stock">Stock</label>
-                    <input type="number" class="form-control" name="stock[]"  placeholder="Enter Product Stock"  required>
-                </div>
-
-                <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
-                    <button type="button" class="btn btn-danger btn-sm" style="margin-top: 26px;" onclick="removeSize(${size_div_count})">Remove</button>
-                </div>
-            </div>`;
+            </div></div>`;
             $("#size_div").append("<span id='sizes"+size_div_count+"'>"+size_html+"</span>");
             size_div_count++;
         });
@@ -220,5 +197,4 @@
  @endsection
 
 
-        
-    
+
--- routes/admin.php
+++ routes/admin.php
@@ -3,49 +3,49 @@
 use Illuminate\Http\Request;
 
 Route::group(['namespace' => 'Admin'],function(){
-    Route::get('/admin/login','LoginController@index')->name('admin.login_form');    
+    Route::get('/admin/login','LoginController@index')->name('admin.login_form');
     Route::post('login', 'LoginController@adminLogin');
 
- 
+
     Route::group(['middleware'=>'auth:admin','prefix'=>'admin'],function(){
-        Route::get('/dashboard', 'DashboardController@dashboardView')->name('admin.deshboard');        
+        Route::get('/dashboard', 'DashboardController@dashboardView')->name('admin.deshboard');
         Route::post('logout', 'LoginController@logout')->name('admin.logout');
         Route::get('/change/password/form', 'DashboardController@changePasswordForm')->name('admin.change_password_form');
         Route::post('/change/password', 'DashboardController@changePassword')->name('admin.change_password');
 
         Route::group(['prefix'=>'category'],function(){
             Route::get('list','CategoryController@categoryList')->name('admin.category_list');
-            Route::get('add/form', 'CategoryController@categoryAddForm')->name('admin.category_add_form');        
+            Route::get('add/form', 'CategoryController@categoryAddForm')->name('admin.category_add_form');
             Route::post('insert/form', 'CategoryController@categoryInsertForm')->name('admin.category_insert_form');
             Route::get('status/{id}/{status}', 'CategoryController@categoryStatus')->name('admin.category_status');
             Route::get('edit/{id}', 'CategoryController@categoryEdit')->name('admin.category_edit');
             Route::put('update/{id}', 'CategoryController@categoryUpdate')->name('admin.category_update');
         });
 
-        Route::group(['prefix'=>'sub/category'],function(){        
+        Route::group(['prefix'=>'sub/category'],function(){
             Route::get('list','CategoryController@subCategoryList')->name('admin.sub_category_list');
-            Route::get('add/form', 'CategoryController@subCategoryAddForm')->name('admin.sub_category_add_form');        
-            Route::post('insert/form', 'CategoryController@subCategoryInsertForm')->name('admin.sub_category_insert_form');    
+            Route::get('add/form', 'CategoryController@subCategoryAddForm')->name('admin.sub_category_add_form');
+            Route::post('insert/form', 'CategoryController@subCategoryInsertForm')->name('admin.sub_category_insert_form');
             Route::get('edit/{id}', 'CategoryController@subCategoryEdit')->name('admin.sub_category_edit');
             Route::put('update/{id}', 'CategoryController@subCategoryUpdate')->name('admin.sub_category_update');
             Route::get('list/with/category/{category_id}', 'CategoryController@subCategoryListWithCategory')->name('admin.sub_category_list_with_category');
             Route::get('status/{id}/{status}', 'CategoryController@subCategoryStatus')->name('admin.sub_category_status');
         });
 
-        Route::group(['prefix'=>'third/category'],function(){        
+        Route::group(['prefix'=>'third/category'],function(){
             Route::get('list','CategoryController@thirdCategoryList')->name('admin.third_category_list');
-            Route::get('add/form', 'CategoryController@thirdCategoryAddForm')->name('admin.third_category_add_form');        
-            Route::post('insert/form', 'CategoryController@thirdCategoryInsertForm')->name('admin.third_category_insert_form');    
+            Route::get('add/form', 'CategoryController@thirdCategoryAddForm')->name('admin.third_category_add_form');
+            Route::post('insert/form', 'CategoryController@thirdCategoryInsertForm')->name('admin.third_category_insert_form');
             Route::get('edit/{id}', 'CategoryController@thirdCategoryEdit')->name('admin.third_category_edit');
             Route::put('update/{id}', 'CategoryController@thirdCategoryUpdate')->name('admin.third_category_update');
             Route::get('list/with/category/{sub_category_id}', 'CategoryController@thirdCategoryListWithSubCategory')->name('admin.third_category_list_with_sub_category');
             Route::get('status/{id}/{status}', 'CategoryController@thirdCategoryStatus')->name('admin.third_category_status');
         });
 
-        Route::group(['prefix'=>'brands'],function(){        
+        Route::group(['prefix'=>'brands'],function(){
             Route::get('list','BrandController@brandList')->name('admin.brand_list');
-            Route::get('add/form', 'BrandController@brandAddForm')->name('admin.brand_add_form');        
-            Route::post('insert/form', 'BrandController@brandInsertForm')->name('admin.brand_insert_form');    
+            Route::get('add/form', 'BrandController@brandAddForm')->name('admin.brand_add_form');
+            Route::post('insert/form', 'BrandController@brandInsertForm')->name('admin.brand_insert_form');
             Route::get('edit/{id}', 'BrandController@brandEdit')->name('admin.brand_edit');
             Route::put('update/{id}', 'BrandController@brandUpdate')->name('admin.brand_update');
             Route::get('list/with/category/{sub_category_id}', 'BrandController@brandListWithSubCategory')->name('admin.brand_list_with_sub_category');
@@ -71,17 +71,17 @@ Route::group(['namespace' => 'Admin'],function(){
             Route::get('status/{id}/{status}', 'CouponController@couponStatus')->name('admin.coupon_status');
         });
 
-        Route::group(['prefix'=>'size'],function(){        
+        Route::group(['prefix'=>'size'],function(){
             Route::get('list','ConfigurationController@sizeList')->name('admin.size_list');
-            Route::get('add/form', 'ConfigurationController@sizeAddForm')->name('admin.size_add_form');        
-            Route::post('insert/form', 'ConfigurationController@sizeInsert')->name('admin.size_insert');    
+            Route::get('add/form', 'ConfigurationController@sizeAddForm')->name('admin.size_add_form');
+            Route::post('insert/form', 'ConfigurationController@sizeInsert')->name('admin.size_insert');
             Route::get('edit/{id}', 'ConfigurationController@sizeEdit')->name('admin.size_edit');
             Route::put('update/{id}', 'ConfigurationController@sizeUpdate')->name('admin.size_update');
             Route::get('list/with/category/{sub_category_id}', 'ConfigurationController@sizeListWithSubCategory')->name('admin.size_list_with_sub_category');
             Route::get('status/{id}/{status}', 'ConfigurationController@sizeStatus')->name('admin.size_status');
         });
 
-        Route::group(['prefix'=>'charges'],function(){ 
+        Route::group(['prefix'=>'charges'],function(){
             Route::get('list','ChargesController@chargesList')->name('admin.charges_list');
             Route::get('edit/{id}', 'ChargesController@chargesEdit')->name('admin.charges_edit');
             Route::put('update/{id}', 'ChargesController@chargesUpdate')->name('admin.charges_update');
@@ -100,9 +100,9 @@ Route::group(['namespace' => 'Admin'],function(){
             Route::get('status/{id}/{status}', 'SliderController@SliderStatus')->name('admin.slider_status');
             Route::get('edit/{id}', 'SliderController@bannerEdit')->name('admin.banner_edit');
             Route::put('update/{id}', 'SliderController@bannerUpdate')->name('admin.banner_update');
-        }); 
+        });
 
-        Route::group(['prefix'=>'product'],function(){       
+        Route::group(['prefix'=>'product'],function(){
             Route::get('add/form','ProductController@AddProductForm')->name('admin.product_add_form');
             Route::get('list','ProductController@ListProducts')->name('admin.list_product');
             Route::post('insert','ProductController@insertProduct')->name('admin.product_insert');
@@ -113,36 +113,34 @@ Route::group(['namespace' => 'Admin'],function(){
             Route::get('status/update/{id}/{status}','ProductController@productStatusUpdate')->name('admin.product_status_update');
 
             Route::get('edit/sizes/{product_id}','ProductController@editSizes')->name('admin.product_edit_sizes');
-            Route::post('add/new/sizes/','ProductController@addNewSize')->name('admin.product_add_new_sizes');
+            Route::put('add/new/sizes/{product_id}','ProductController@addNewSize')->name('admin.product_add_new_sizes');
             Route::put('update/sizes/{product_id}','ProductController@updateSize')->name('admin.product_update_sizes');
+
             Route::get('edit/color/{id}','ProductController@productEditColors')->name('admin.product_edit_colors');
             Route::put('update/color/{product_id}','ProductController@updateColor')->name('admin.product_update_color');
-            Route::post('add/new/colors/{product_id}','ProductController@addNewColor')->name('admin.product_add_new_colors');
+            Route::put('add/new/colors/{product_id}','ProductController@addNewColor')->name('admin.product_add_new_colors');
             Route::get('delete/color/{product_color_id}','ProductController@productDeleteColor')->name('admin.delete_product_color');
-            // Route::get('edit/sizes/{product_id}','ProductController@editSizes')->name('admin.product_edit_sizes');
-            // Route::post('add/new/sizes/','ProductController@addNewSize')->name('admin.product_add_new_sizes');
-            // Route::post('update/sizes/','ProductController@updateSize')->name('admin.product_update_sizes');
-            
+
             // Route::get('edit/specifications/{product_id}','ProductController@editSpecifications')->name('admin.product_edit_specifications');
             // Route::post('add/new/specofication/','ProductController@addNewSpecification')->name('admin.product_add_new_specofication');
             // Route::post('update/specofication/','ProductController@updateSpecification')->name('admin.product_update_specofication');
             // Route::get('delete/specofication/{sp_id}','ProductController@deleteSpecification')->name('admin.product_delete_specofication');
 
             Route::get('edit/images/{product_id}','ProductController@editImages')->name('admin.product_edit_images');
-            Route::post('add/new/images/','ProductController@addNewImages')->name('admin.product_add_new_images');            
-            Route::get('make/cover/image/{product_id}/{image_id}','ProductController@makeCoverImage')->name('admin.product_make_cover_image');            
+            Route::post('add/new/images/','ProductController@addNewImages')->name('admin.product_add_new_images');
+            Route::get('make/cover/image/{product_id}/{image_id}','ProductController@makeCoverImage')->name('admin.product_make_cover_image');
             Route::get('delete/image/{image_id}','ProductController@deleteImage')->name('admin.product_delete_image');
         });
 
         Route::group(['prefix'=>'user'],function(){
-            Route::get('list','UserController@userList')->name('admin.user_list');            
+            Route::get('list','UserController@userList')->name('admin.user_list');
             Route::get('list/ajax','UserController@userListAjax')->name('admin.user_list_ajax');
         });
 
         // Route::group(['prefix'=>'order'],function(){
-        //     Route::get('/list','OrderController@orderList')->name('admin.order_list');            
+        //     Route::get('/list','OrderController@orderList')->name('admin.order_list');
         //     Route::get('/list/ajax','OrderController@orderListAjax')->name('admin.order_list_ajax');
         // });
-        
+
     });
 });

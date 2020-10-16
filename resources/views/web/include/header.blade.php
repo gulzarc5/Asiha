f9dea5c71d6ab41183226d422655c0bc524fc588
atikrex <atikrex@gmail.com>
2020-10-16 14:13:35 +0530

    updated

--- resources/views/web/include/header.blade.php
+++ resources/views/web/include/header.blade.php
@@ -140,10 +140,13 @@
                                     </li>
                                 @endif
                             @endforeach
-
+                            
 
                             @auth('user')
-                                <li><a href="{{route('web.dashboard')}}"><span class="menu-text"><i class="fa fa-user"></i> Hello, <b>{{$user_data->name}}</b></span></a> </li>
+                                @php
+                                    $first_name = explode(" ",$user_data->name);
+                                @endphp
+                                <li><a href="{{route('web.dashboard')}}"><span class="menu-text"><i class="fa fa-user"></i> Hello, <b>{{$first_name[0]}}</b></span></a> </li>
                             @else
                                 <li class="has-children"><a href="#"><span class="menu-text"><i class="fal fa-user"></i> Login/Register</span></a>
                                     <ul class="sub-menu">

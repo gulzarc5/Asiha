2bf60fd603837f1386521f225445558758893685
gulzarc5 <gulzarc5@gmail.com>
2020-10-20 16:17:05 +0530

    update

--- app/Http/Controllers/Admin/ChargesController.php
+++ app/Http/Controllers/Admin/ChargesController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\charges;
+use App\Models\Charges;
 class ChargesController extends Controller
 {
     public function chargesList()
     {
         $charges = Charges::orderBy('id','desc')->get();
-        return view('admin.charges.charges_list',compact('charges')); 
+        return view('admin.charges.charges_list',compact('charges'));
     }
 
     public function chargesEdit($charges_id)
@@ -21,23 +21,23 @@ class ChargesController extends Controller
             return redirect()->back();
         }
         $charges = Charges::where('id',$id)->first();
-        
+
         return view('admin.charges.edit_charges',compact('charges'));
     }
 
     public function chargesUpdate(Request $request,$id)
-    {   
+    {
         $this->validate($request, [
             'amount'   => 'required',
-            
+
         ]);
         Charges::where('id',$id)
             ->update([
                 'amount'=>$request->input('amount'),
-               
+
         ]);
         return redirect()->back()->with('message','Charges Updated Successfully');
-        
+
     }
 
     public function chargesStatus($id,$status)
